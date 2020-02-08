263c99c7bde02663ad6433513b05100831d7e0fa
Aaron Rinn <arinn1250@gmail.com>
2020-02-07 20:09:32 -0800

    initial commit

---
+++ Silo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Silo
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

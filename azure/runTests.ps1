bc1b8001f56704d64a949a1033148b9829e37543
Aaron Rinn <arinn1250@gmail.com>
2020-02-11 18:31:57 -0800

    added file extension

--- azure/runTests.ps1
+++ azure/runTests.ps1
@@ -26,7 +26,7 @@ foreach($testProject in $tests) {
         throw 'Build failed. See build output'
     }
 
-    dotnet test -c $Configuration --no-build --logger:trx /p:CollectCoverage=true $testProject
+    dotnet test -c $Configuration --no-build --logger:trx /p:CollectCoverage=true "$testProject.csproj"
     if (!$?) {
         Pop-Location
         throw 'Test failed. See test output'

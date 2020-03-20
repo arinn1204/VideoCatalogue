bcb332d212cc206156b56c5ed4b49d77b46a2156
Aaron Rinn <arinn1250@gmail.com>
2020-03-20 06:46:56 -0700

    added sql parameters to test script

--- azure/runTests.ps1
+++ azure/runTests.ps1
@@ -1,9 +1,14 @@
 ﻿param (
     [string] $type, # this is the type of test, ie unit vs integration, vs e2e etc
+    [string] $useEnvironmentConnectionString = 'false',
     [string] $projectLocation = '.',
     [string] $Configuration = 'Release',
     [string] $DebugPreference = 'Continue',
-    [string] $TheMovieDatabaseKey = ''
+    [string] $TheMovieDatabaseKey = '',
+    [string] $dbCatalog = '',
+    [string] $dbUsername = '',
+    [string] $dbPassword = '',
+    [string] $dbSource = ''
 )
 
 Push-Location $projectLocation\Tests
@@ -17,6 +22,11 @@ switch -regex ($type) {
 $tests = Get-ChildItem | Where-Object {$_.Name -match $projectEnd }
 
 $Env:TheMovieDatabase:Authorization = $TheMovieDatabaseKey
+$Env:environment_connection_string = $useEnvironmentConnectionString;
+$Env:db_source = $dbSource;
+$Env:db_catalog = $dbCatalog;
+$Env:db_username = $dbUsername;
+$Env:db_password = $dbPassword;
 
 foreach($testProject in $tests) {
     Push-Location $testProject

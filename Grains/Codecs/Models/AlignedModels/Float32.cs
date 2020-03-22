aa5c0fb0c448f112b3c983507408edc20d428a52
Aaron Rinn <arinn1250@gmail.com>
2020-03-21 19:19:00 -0700

    Moved to own namespace

---
+++ Grains/Codecs/Models/AlignedModels/Float32.cs
@@ -0,0 +1,26 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Grains.Codecs.Models.AlignedModels
+{
+	[StructLayout(LayoutKind.Explicit)]
+	public struct Float32
+	{
+		[FieldOffset(0)]
+		public byte B1;
+
+		[FieldOffset(1)]
+		public byte B2;
+
+		[FieldOffset(2)]
+		public byte B3;
+
+		[FieldOffset(3)]
+		public byte B4;
+
+		[FieldOffset(0)]
+		public float Data;
+
+		[FieldOffset(0)]
+		public uint UnsignedData;
+	}
+}
---
+++ Grains/Codecs/Models/AlignedModels/Float64.cs
@@ -0,0 +1,38 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Grains.Codecs.Models.AlignedModels
+{
+	[StructLayout(LayoutKind.Explicit)]
+	public struct Float64
+	{
+		[FieldOffset(0)]
+		public byte B1;
+
+		[FieldOffset(1)]
+		public byte B2;
+
+		[FieldOffset(2)]
+		public byte B3;
+
+		[FieldOffset(3)]
+		public byte B4;
+		
+		[FieldOffset(4)]
+		public byte B5;
+
+		[FieldOffset(5)]
+		public byte B6;
+
+		[FieldOffset(6)]
+		public byte B7;
+
+		[FieldOffset(7)]
+		public byte B8;
+
+		[FieldOffset(0)]
+		public double Data;
+
+		[FieldOffset(0)]
+		public ulong UnsignedData;
+	}
+}

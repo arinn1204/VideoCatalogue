d5bba224b451b099facef9872e7aa640187066ce
Aaron Rinn <arinn1250@gmail.com>
2020-03-22 11:04:06 -0700

    Split EBML specific logic to utility class

---
+++ Grains/Codecs/ExtensibleBinaryMetaLanguage/Ebml.cs
@@ -0,0 +1,72 @@
+﻿using System.IO;
+using Grains.Codecs.Models.AlignedModels;
+
+namespace Grains.Codecs.ExtensibleBinaryMetaLanguage
+{
+	public static class Ebml
+	{
+		public static uint GetId(Stream stream)
+		{
+			var firstByte = (byte) stream.ReadByte();
+			var word = new Float32
+			           {
+				           B4 = firstByte,
+				           B3 = (byte) stream.ReadByte(),
+				           B2 = (byte) stream.ReadByte(),
+				           B1 = (byte) stream.ReadByte()
+			           };
+
+			return word.UnsignedData;
+		}
+
+		public static (int width, long size) GetWidthAndSize(Stream stream)
+		{
+			var firstByte = (byte) stream.ReadByte();
+			var width = GetWidth(firstByte);
+
+			var result = width switch
+			             {
+				             8 => ReadBytes(stream, (long) stream.ReadByte() << 48, 6),
+				             7 => ReadBytes(stream, (long) firstByte << 48, 6),
+				             6 => ReadBytes(stream, ((long) firstByte & 3) << 40, 5),
+				             5 => ReadBytes(stream, ((long) firstByte & 7) << 32, 4),
+				             4 => ReadBytes(stream, ((long) firstByte & 15) << 24, 3),
+				             3 => ReadBytes(stream, ((long) firstByte & 31) << 16, 2),
+				             2 => ReadBytes(stream, ((long) firstByte & 63) << 8, 1),
+				             1 => ReadBytes(stream, firstByte & 127, 0),
+				             _ => 0L
+			             };
+
+			return (width, result);
+		}
+
+
+		private static long ReadBytes(Stream stream, long seed, int bytesToRead)
+		{
+			var result = seed;
+			for (var i = bytesToRead; i > 0; i--)
+			{
+				var multiplier = (i - 1) * 8;
+				result += (long) stream.ReadByte() << multiplier;
+			}
+
+			return result;
+		}
+
+
+		private static int GetWidth(byte firstByte)
+		{
+			byte result = 0;
+			var first = 255;
+
+			while (first != 0)
+			{
+				if ((firstByte | first) == first)
+					result++;
+				first >>= 1;
+			}
+
+			return result;
+		}
+	}
+}
---
+++ Tests/Grains.Tests.Unit/Codecs/EbmlTests.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.IO;
+using System.Runtime.InteropServices;
+using FluentAssertions;
+using Grains.Codecs.ExtensibleBinaryMetaLanguage;
+using Xunit;
+
+namespace Grains.Tests.Unit.Codecs
+{
+	public class EbmlTests
+	{
+		[Fact]
+		public void ShouldReturnId()
+		{
+			var firstByte = Convert.ToByte("1A", 16);
+			var secondByte = Convert.ToByte("45", 16);
+			var thirdByte = Convert.ToByte("DF", 16);
+			var fourthByte = Convert.ToByte("A3", 16);
+
+			using var stream = new MemoryStream();
+			using var writer = new BinaryWriter(stream);
+			writer.Write(firstByte);
+			writer.Write(secondByte);
+			writer.Write(thirdByte);
+			writer.Write(fourthByte);
+			writer.Flush();
+
+			stream.Position = 0;
+
+			Ebml.GetId(stream)
+			    .Should()
+			    .Be(Convert.ToUInt32("0x1A45DFA3", 16));
+		}
+
+		[Theory]
+		[InlineData(8, 5367889050668557)]
+		[InlineData(7, 865393246736142)]
+		[InlineData(6, 3380442370063)]
+		[InlineData(5, 30384722192)]
+		[InlineData(4, 252908049)]
+		[InlineData(3, 2036498)]
+		[InlineData(2, 16147)]
+		[InlineData(1, 127)]
+		public void ShouldReturnWidthAndSize(int expectedWidth, long expectedSize)
+		{
+			var width = expectedWidth switch
+			            {
+				            8 => 1,
+				            7 => 3,
+				            6 => 7,
+				            5 => 15,
+				            4 => 31,
+				            3 => 63,
+				            2 => 127,
+				            1 => 255
+			            };
+			using var stream = new MemoryStream();
+			using var writer = new BinaryWriter(stream);
+
+			var junkData = new[]
+			               {
+				               (byte) width,
+				               (byte) 19,
+				               (byte) 18,
+				               (byte) 17,
+				               (byte) 16,
+				               (byte) 15,
+				               (byte) 14,
+				               (byte) 13,
+				               (byte) 12
+			               };
+			
+			writer.Write(junkData);
+			writer.Flush();
+
+			stream.Position = 0;
+
+			var result = Ebml.GetWidthAndSize(stream);
+
+			result.Should()
+			      .Be((expectedWidth, expectedSize));
+		}
+	}
+}

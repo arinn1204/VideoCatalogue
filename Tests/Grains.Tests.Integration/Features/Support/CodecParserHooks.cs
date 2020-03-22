98a3b9efa33e74e632d9cc3d51406d0f64fbbcda
Aaron Rinn <arinn1250@gmail.com>
2020-03-22 14:59:50 -0700

    added new ebml class

--- Tests/Grains.Tests.Integration/Features/Support/CodecParserHooks.cs
+++ Tests/Grains.Tests.Integration/Features/Support/CodecParserHooks.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using BoDi;
 using Grains.Codecs;
+using Grains.Codecs.ExtensibleBinaryMetaLanguage;
+using Grains.Codecs.ExtensibleBinaryMetaLanguage.Interfaces;
 using Grains.Codecs.Matroska;
 using Grains.Codecs.Matroska.Interfaces;
 using Microsoft.Extensions.DependencyInjection;
@@ -22,6 +24,7 @@ namespace Grains.Tests.Integration.Features.Support
 					"https://raw.githubusercontent.com/Matroska-Org/foundation-source/master/spectool/specdata.xml"));
 			serviceCollection.AddTransient<ISpecification, Specification>();
 			serviceCollection.AddTransient<IMatroska, Matroska>();
+			serviceCollection.AddTransient<IEbml, Ebml>();
 			serviceCollection.AddTransient<Parser, Parser>();
 
 			objectContainer.RegisterInstanceAs(

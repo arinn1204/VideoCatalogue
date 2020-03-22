9a0a7bfb8baf00859bac9f6464a872770b28b879
Aaron Rinn <arinn1250@gmail.com>
2020-03-21 19:35:00 -0700

    Added interface for the spec

---
+++ Grains/Codecs/Matroska/Interfaces/ISpecification.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using Grains.Codecs.Matroska.Models;
+
+namespace Grains.Codecs.Matroska.Interfaces
+{
+	public interface ISpecification
+	{
+		Task<MatroskaSpecification> GetSpecification();
+	}
+}

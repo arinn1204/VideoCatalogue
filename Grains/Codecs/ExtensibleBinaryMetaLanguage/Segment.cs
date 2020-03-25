d559faf17bd64173b60cb0509dea6ad817b54f2c
Aaron Rinn <arinn1250@gmail.com>
2020-03-25 06:35:17 -0700

    Renamed from MatroskaSegment to Segment as its not specific to matroska specification.

---
+++ Grains/Codecs/ExtensibleBinaryMetaLanguage/Segment.cs
@@ -0,0 +1,53 @@
+﻿using System.IO;
+using System.Linq;
+using Grains.Codecs.ExtensibleBinaryMetaLanguage.Interfaces;
+using Grains.Codecs.ExtensibleBinaryMetaLanguage.Utilities;
+using Grains.Codecs.Matroska.Models;
+
+namespace Grains.Codecs.ExtensibleBinaryMetaLanguage
+{
+	public class Segment : ISegment
+	{
+		private readonly IEbml _ebml;
+		private readonly ISegmentFactory _segmentFactory;
+
+		public Segment(IEbml ebml, ISegmentFactory segmentFactory)
+		{
+			_ebml = ebml;
+			_segmentFactory = segmentFactory;
+		}
+
+		public SegmentInformation GetSegmentInformation(
+			Stream stream,
+			MatroskaSpecification matroskaSpecification)
+		{
+			var totalSize = EbmlReader.GetSize(stream);
+			var endPosition = stream.Position + totalSize;
+			var trackedElements = matroskaSpecification.Elements
+			                                           .Where(w => w.Level == 1)
+			                                           .ToDictionary(
+				                                            k => k.Id,
+				                                            v => v.Name);
+
+			var segmentInformation = new SegmentInformation();
+
+			while (stream.Position < endPosition)
+			{
+				var id = _ebml.GetMasterIds(stream, matroskaSpecification);
+				var size = EbmlReader.GetSize(stream);
+
+				if (trackedElements.ContainsKey(id))
+				{
+					var trackedElement = trackedElements[id];
+					var child = _segmentFactory.GetChild(trackedElement);
+					var childInformation = child.GetChildInformation(stream, matroskaSpecification);
+					segmentInformation = child.Merge(segmentInformation, childInformation);
+				}
+
+				stream.Seek(size, SeekOrigin.Current);
+			}
+
+			return segmentInformation;
+		}
+	}
+}

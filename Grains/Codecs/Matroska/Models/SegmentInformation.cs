ffed74780b247f3970e85eb937a2ba558e92402c
Aaron Rinn <arinn1250@gmail.com>
2020-03-24 17:44:12 -0700

    Added constructor to initialize enumerables

--- Grains/Codecs/Matroska/Models/SegmentInformation.cs
+++ Grains/Codecs/Matroska/Models/SegmentInformation.cs
@@ -1,10 +1,18 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using GrainsInterfaces.Models.CodecParser;
 
 namespace Grains.Codecs.Matroska.Models
 {
 	public class SegmentInformation
 	{
+		public SegmentInformation()
+		{
+			Videos = Enumerable.Empty<VideoInformation>();
+			Audios = Enumerable.Empty<AudioInformation>();
+			Subtitles = Enumerable.Empty<Subtitle>();
+		}
+
 		public IEnumerable<VideoInformation> Videos { get; set; }
 		public IEnumerable<AudioInformation> Audios { get; set; }
 		public IEnumerable<Subtitle> Subtitles { get; set; }

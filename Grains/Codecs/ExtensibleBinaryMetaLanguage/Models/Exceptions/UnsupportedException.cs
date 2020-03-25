7e9dd89586be68d35f3f1a187368a9eef19e7c56
Aaron Rinn <arinn1250@gmail.com>
2020-03-25 07:29:40 -0700

    initial commit

---
+++ Grains/Codecs/ExtensibleBinaryMetaLanguage/Models/Exceptions/UnsupportedException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Grains.Codecs.ExtensibleBinaryMetaLanguage.Models.Exceptions
+{
+	public class UnsupportedException : Exception
+	{
+		public UnsupportedException(string message)
+			: base(message)
+		{
+		}
+	}
+}

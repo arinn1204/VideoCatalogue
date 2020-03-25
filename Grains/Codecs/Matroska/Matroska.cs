b620df11e074e244191f531d6ae9d94723a39c4f
Aaron Rinn <arinn1250@gmail.com>
2020-03-24 17:44:33 -0700

    Added while loop to iterate all segments (realistically should only ever be one)

--- Grains/Codecs/Matroska/Matroska.cs
+++ Grains/Codecs/Matroska/Matroska.cs
@@ -2,6 +2,7 @@
 using System.IO;
 using System.Linq;
 using Grains.Codecs.ExtensibleBinaryMetaLanguage.Interfaces;
+using Grains.Codecs.ExtensibleBinaryMetaLanguage.Utilities;
 using Grains.Codecs.Matroska.Interfaces;
 using Grains.Codecs.Matroska.Models;
 using GrainsInterfaces.Models.CodecParser;
@@ -12,18 +13,18 @@ namespace Grains.Codecs.Matroska
 	{
 		private readonly IEbml _ebml;
 		private readonly Lazy<(uint ebml, uint segment)> _ebmlAndSegmentId;
-		private readonly IMatroskaSegment _matroskaSegment;
+		private readonly ISegment _segment;
 		private readonly Lazy<MatroskaSpecification> _matroskaSpecification;
 
 		public Matroska(
 			ISpecification specification,
 			IEbml ebml,
-			IMatroskaSegment matroskaSegment)
+			ISegment segment)
 		{
 			_ =
 				specification ?? throw new ArgumentNullException(nameof(specification));
 			_ebml = ebml;
-			_matroskaSegment = matroskaSegment;
+			_segment = segment;
 			_matroskaSpecification =
 				new Lazy<MatroskaSpecification>(
 					() => specification.GetSpecification()
@@ -84,9 +85,21 @@ namespace Grains.Codecs.Matroska
 				return default;
 			}
 
-			var segmentInformation = _matroskaSegment.GetSegmentInformation(
-				stream,
-				_matroskaSpecification.Value);
+			var segmentInformation = new SegmentInformation();
+
+			while ((id = _ebml.GetMasterIds(stream, _matroskaSpecification.Value)) ==
+			       _ebmlAndSegmentId.Value.segment)
+			{
+				var segment = _segment.GetSegmentInformation(
+					stream,
+					_matroskaSpecification.Value);
+
+				segmentInformation.Audios = segmentInformation.Audios.Concat(segment.Audios);
+				segmentInformation.Videos = segmentInformation.Videos.Concat(segment.Videos);
+				segmentInformation.Subtitles =
+					segmentInformation.Subtitles.Concat(segment.Subtitles);
+			}
+
 			return new FileInformation
 			       {
 				       Container = ebmlHeader.DocType,

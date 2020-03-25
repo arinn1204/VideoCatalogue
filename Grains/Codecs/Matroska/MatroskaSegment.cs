7f5a24d9cda52cbf6a45e097fed883f73a982684
Aaron Rinn <arinn1250@gmail.com>
2020-03-24 18:04:02 -0700

    initial commit

---
+++ Grains/Codecs/Matroska/MatroskaSegment.cs
@@ -0,0 +1,33 @@
+﻿using System.IO;
+using Grains.Codecs.ExtensibleBinaryMetaLanguage.Interfaces;
+using Grains.Codecs.ExtensibleBinaryMetaLanguage.Utilities;
+using Grains.Codecs.Matroska.Interfaces;
+using Grains.Codecs.Matroska.Models;
+
+namespace Grains.Codecs.Matroska
+{
+	public class MatroskaSegment : ISegment
+	{
+		private readonly IEbml _ebml;
+
+		public MatroskaSegment(IEbml ebml)
+		{
+			_ebml = ebml;
+		}
+
+		public SegmentInformation GetSegmentInformation(
+			Stream stream,
+			MatroskaSpecification matroskaSpecification)
+		{
+			var size = EbmlReader.GetSize(stream);
+			var endPosition = stream.Position + size;
+
+			while (stream.Position < endPosition)
+			{
+				var id = _ebml.GetMasterIds(stream, matroskaSpecification);
+			}
+			
+			return new SegmentInformation();
+		}
+	}
+}

86b926cd80a16113742bdc14e2e34e7802cffd78
Aaron Rinn <arinn1250@gmail.com>
2020-03-28 12:13:03 -0700

    Added deserializer attributes

--- Grains/Codecs/ExtensibleBinaryMetaLanguage/Models/ChapterTranslate.cs
+++ Grains/Codecs/ExtensibleBinaryMetaLanguage/Models/ChapterTranslate.cs
@@ -1,9 +1,17 @@
-﻿namespace Grains.Codecs.ExtensibleBinaryMetaLanguage.Models
+﻿using Grains.Codecs.ExtensibleBinaryMetaLanguage.Attributes;
+
+namespace Grains.Codecs.ExtensibleBinaryMetaLanguage.Models
 {
+	[EbmlMaster]
 	public class ChapterTranslate
 	{
+		[EbmlElement("ChapterTranslateEditionUID")]
 		public ulong? ChapterTranslateEditionUID { get; set; }
+
+		[EbmlElement("ChapterTranslateCodec")]
 		public uint ChapterTranslateCodec { get; set; }
+
+		[EbmlElement("ChapterTranslateID")]
 		public uint ChapterTranslateID { get; set; }
 	}
 }
--- Grains/Codecs/ExtensibleBinaryMetaLanguage/Models/Info.cs
+++ Grains/Codecs/ExtensibleBinaryMetaLanguage/Models/Info.cs
@@ -1,20 +1,34 @@
 ﻿#nullable enable
 using System;
+using Grains.Codecs.ExtensibleBinaryMetaLanguage.Attributes;
 
 namespace Grains.Codecs.ExtensibleBinaryMetaLanguage.Models
 {
+	[EbmlMaster]
 	public class Info
 	{
+		[EbmlElement("SegmentUID")]
 		public ulong? SegmentUID { get; set; }
+
 		public string? SegmentFilename { get; set; }
+
+		[EbmlElement("PrevUID")]
 		public ulong? PrevUID { get; set; }
+
+		[EbmlElement("PrevFilename")]
 		public string? PrevFilename { get; set; }
+
+		[EbmlElement("NextUID")]
 		public ulong? NextUID { get; set; }
+
 		public string? NextFilename { get; set; }
 		public ulong? SegmentFamily { get; set; }
 		public uint TimecodeScale { get; set; }
 		public float? Duration { get; set; }
+
+		[EbmlElement("DateUTC")]
 		public DateTime? DateUTC { get; set; }
+
 		public string? Title { get; set; }
 		public string MuxingApp { get; set; }
 		public string WritingApp { get; set; }

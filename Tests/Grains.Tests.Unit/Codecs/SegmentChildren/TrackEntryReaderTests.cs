080968e8a52a27b57c5d9459393d91f31727207d
Aaron Rinn <arinn1250@gmail.com>
2020-03-30 05:03:53 -0700

    initial commit

---
+++ Tests/Grains.Tests.Unit/Codecs/SegmentChildren/TrackEntryReaderTests.cs
@@ -0,0 +1,27 @@
+﻿using AutoFixture;
+using AutoFixture.AutoMoq;
+using Grains.Codecs.ExtensibleBinaryMetaLanguage.Interfaces;
+using Grains.Codecs.ExtensibleBinaryMetaLanguage.Models;
+using Grains.Codecs.ExtensibleBinaryMetaLanguage.SegmentChildren.Tracks;
+using Grains.Tests.Unit.Fixtures;
+using Grains.Tests.Unit.TestUtilities;
+using Xunit;
+
+namespace Grains.Tests.Unit.Codecs.SegmentChildren
+{
+	public class TrackEntryReaderTests : IClassFixture<MatroskaFixture>
+	{
+		private readonly Fixture _fixture;
+
+		private readonly EbmlSpecification _specification;
+
+		public TrackEntryReaderTests(MatroskaFixture fixture)
+		{
+			_specification = fixture.Specification;
+			_fixture = new Fixture();
+			_fixture.Customize(new AutoMoqCustomization());
+			_fixture.Register<ITrackEntryReader>(() => _fixture.Create<TrackEntryReader>());
+			_fixture.Inject(MapperHelper.CreateMapper());
+		}
+	}
+}

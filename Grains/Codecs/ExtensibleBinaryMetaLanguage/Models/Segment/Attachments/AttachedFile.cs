199fae2cc48cc23100816ded9560d8fc350eccc6
Aaron Rinn <arinn1250@gmail.com>
2020-04-03 05:04:55 -0700

    Added attachment models

---
+++ Grains/Codecs/ExtensibleBinaryMetaLanguage/Models/Segment/Attachments/AttachedFile.cs
@@ -0,0 +1,15 @@
+﻿using Grains.Codecs.ExtensibleBinaryMetaLanguage.Attributes;
+
+namespace Grains.Codecs.ExtensibleBinaryMetaLanguage.Models.Segment.Attachments
+{
+	[EbmlMaster]
+	public class AttachedFile
+	{
+		public string FileDescription { get; set; }
+		public string FileName { get; set; }
+		public string FileMimeType { get; set; }
+
+		[EbmlElement("FileUid")]
+		public uint FileUid { get; set; }
+	}
+}

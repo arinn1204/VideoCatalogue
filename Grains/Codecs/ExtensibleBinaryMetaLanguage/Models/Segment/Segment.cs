ed78362dd8a4d6e3a8573a0873b5618417189da3
Aaron Rinn <arinn1250@gmail.com>
2020-04-04 06:30:35 -0700

    fully populated segment model with the correct elements

--- Grains/Codecs/ExtensibleBinaryMetaLanguage/Models/Segment/Segment.cs
+++ Grains/Codecs/ExtensibleBinaryMetaLanguage/Models/Segment/Segment.cs
@@ -1,5 +1,12 @@
-﻿using Grains.Codecs.ExtensibleBinaryMetaLanguage.Attributes;
+﻿using System.Collections.Generic;
+using Grains.Codecs.ExtensibleBinaryMetaLanguage.Attributes;
+using Grains.Codecs.ExtensibleBinaryMetaLanguage.Models.Segment.Attachments;
+using Grains.Codecs.ExtensibleBinaryMetaLanguage.Models.Segment.Chapters;
+using Grains.Codecs.ExtensibleBinaryMetaLanguage.Models.Segment.Clusters;
+using Grains.Codecs.ExtensibleBinaryMetaLanguage.Models.Segment.Cues;
+using Grains.Codecs.ExtensibleBinaryMetaLanguage.Models.Segment.MetaSeekInformation;
 using Grains.Codecs.ExtensibleBinaryMetaLanguage.Models.Segment.SegmentInformation;
+using Grains.Codecs.ExtensibleBinaryMetaLanguage.Models.Segment.Tags;
 using Grains.Codecs.ExtensibleBinaryMetaLanguage.Models.Segment.Tracks;
 
 namespace Grains.Codecs.ExtensibleBinaryMetaLanguage.Models.Segment
@@ -7,12 +14,24 @@ namespace Grains.Codecs.ExtensibleBinaryMetaLanguage.Models.Segment
 	[EbmlMaster]
 	public class Segment
 	{
-		public SeekHead.SeekHead SeekHead { get; set; }
+		public SegmentCues? Cues { get; set; }
+		public IEnumerable<Track>? Tracks { get; set; }
+		public IEnumerable<SegmentTag>? Tags { get; set; }
 
-		[EbmlElement("Tracks")]
-		public Track TrackInformation { get; set; }
+		[EbmlElement("Attachments")]
+		public SegmentAttachments? Attachment { get; set; }
+
+		[EbmlElement("Chapters")]
+		public SegmentChapter? Chapter { get; set; }
+
+
+		[EbmlElement("SeekHead")]
+		public IEnumerable<SeekHead>? SeekHeads { get; set; }
 
 		[EbmlElement("Info")]
-		public Info SegmentInformation { get; set; }
+		public IEnumerable<Info> SegmentInformations { get; set; }
+
+		[EbmlElement("Cluster")]
+		public IEnumerable<SegmentCluster>? Clusters { get; set; }
 	}
 }

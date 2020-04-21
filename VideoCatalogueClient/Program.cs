3326c157560258bccabee17eb0a53177d6dbb7ef
Aaron Rinn <arinn1250@gmail.com>
2020-04-20 18:49:59 -0700

    Updated to .net core 3.1 Updated Orleans packages Added MVC files as azure does not support GRPC so main interface will be restful

--- VideoCatalogueClient/Program.cs
+++ VideoCatalogueClient/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
 
 namespace VideoCatalogueClient
 {
@@ -6,7 +7,15 @@ namespace VideoCatalogueClient
 	{
 		public static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			CreateHostBuilder(args).Build().Run();
 		}
+
+		public static IHostBuilder CreateHostBuilder(string[] args) =>
+			Host.CreateDefaultBuilder(args)
+			    .ConfigureWebHostDefaults(
+				     webBuilder =>
+				     {
+					     webBuilder.UseStartup<Startup>();
+				     });
 	}
 }

13f2c08f5bd8b1bcf04ef4710c232583d104eb6c
Aaron Rinn <arinn1250@gmail.com>
2020-04-28 06:48:54 -0700

    Made the action step specify the client Added wiremock stubs for transmission

--- Tests/Grains.Tests.Integration/Features/AClientWantingToSeeAvailableTorrents.feature
+++ Tests/Grains.Tests.Integration/Features/AClientWantingToSeeAvailableTorrents.feature
@@ -4,5 +4,5 @@ Feature: AClientWantingToSeeAvailableTorrents
   @Transmission
   Scenario: A client wants to see what torrents are currently seeding
     Given 1 active seeding torrent
-    When the client goes to view the active torrents
+    When the client goes to view the active torrents in transmission
     Then the client sees 1 active seeding torrent
--- Tests/Grains.Tests.Integration/Features/Actions/BitTorrentAction.cs
+++ Tests/Grains.Tests.Integration/Features/Actions/BitTorrentAction.cs
@@ -1,8 +1,19 @@
-﻿using System.Linq;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text.Json;
 using System.Threading.Tasks;
+using AutoBogus;
+using Grains.BitTorrent.Transmission.Models;
 using Grains.Tests.Integration.Features.Models;
 using GrainsInterfaces.BitTorrent;
+using GrainsInterfaces.BitTorrent.Models;
+using MoreLinq;
 using TechTalk.SpecFlow;
+using WireMock.RequestBuilders;
+using WireMock.ResponseBuilders;
+using WireMock.Server;
 
 namespace Grains.Tests.Integration.Features.Actions
 {
@@ -11,21 +22,103 @@ namespace Grains.Tests.Integration.Features.Actions
 	{
 		private readonly IBitTorrentClient _bitTorrentClient;
 		private readonly BitTorrentData _data;
+		private readonly WireMockServer _wiremock;
 
 		public BitTorrentAction(
 			IBitTorrentClient bitTorrentClient,
-			BitTorrentData data)
+			BitTorrentData data,
+			WireMockServer wiremock)
 		{
 			_bitTorrentClient = bitTorrentClient;
 			_data = data;
+			_wiremock = wiremock;
 		}
 
-		[When(@"the client goes to view the active torrents")]
-		public async Task TheClientGoesToViewActiveTorrents()
+		[When(@"the client goes to view the active torrents in (.*)")]
+		public async Task TheClientGoesToViewActiveTorrents(string btClient)
 		{
+			var data = SetStubs(_wiremock, _data, btClient);
 			var activeTorrents = await _bitTorrentClient.GetActiveTorrents();
 			var response = await activeTorrents.ToListAsync();
 			_data.Response = response;
+		}
+
+		private string SetStubs(WireMockServer wiremock, BitTorrentData data, string btClient)
+		{
+			var (path, body) = btClient switch
+			                   {
+				                   "transmission" => ("/transmission/rpc", Serialize(data)),
+				                   _ => throw new NotImplementedException(
+					                   $"'{btClient}' is not yet supported.")
+			                   };
+
+			wiremock.Given(
+				         Request.Create()
+				                .UsingPost()
+				                .WithPath(path))
+			        .RespondWith(
+				         Response.Create()
+				                 .WithHeader("X-Transmission-Session-Id", "some value")
+				                 .WithStatusCode(HttpStatusCode.OK)
+				                 .WithBody(body));
+
+			return body;
+		}
+
+		private string Serialize(BitTorrentData data)
+		{
+			var responses = new List<TorrentResponse>();
+			var statuses = data.Status.FullJoin(
+				data.QueuedStatus,
+				left => left.ToString(),
+				right => right.ToString(),
+				left => (left, null as QueuedStatus?),
+				right => (null as TorrentStatus?, right),
+				(left, right)
+					=> (left as TorrentStatus?, right as QueuedStatus?));
+
+			foreach (var (torrentStatus, queuedStatus) in statuses)
+			{
+				var status = (torrentStatus, queuedStatus) switch
+				             {
+					             (TorrentStatus.Stopped, _)                      => 0,
+					             (TorrentStatus.Queued, QueuedStatus.CheckFiles) => 1,
+					             (TorrentStatus.CheckingFiles, _)                => 2,
+					             (TorrentStatus.Queued, QueuedStatus.Download)   => 3,
+					             (TorrentStatus.Downloading, _)                  => 4,
+					             (TorrentStatus.Queued, QueuedStatus.Seed)       => 5,
+					             (TorrentStatus.Seeding, _)                      => 6,
+					             _                                               => -1
+				             };
+				var responseObject = new AutoFaker<TorrentResponse>()
+				                    .RuleFor(
+					                     r => r.Files,
+					                     r => new AutoFaker<FileResponse>()
+					                         .RuleFor(
+						                          fileResponse => fileResponse.Length,
+						                          _ => 1000UL)
+					                         .RuleFor(
+						                          fileResponse => fileResponse.BytesCompleted,
+						                          _ => 1000UL)
+					                         .Generate(r.Random.Int(1, 15)))
+				                    .RuleFor(
+					                     r => r.Status,
+					                     _ => status)
+				                    .Generate();
+
+				responses.Add(responseObject);
+			}
+
+			var transmissionResponse =
+				new TransmissionResponse
+				{
+					ResponseArguments = new ResponseArguments
+					                    {
+						                    TorrentResponses = responses
+					                    }
+				};
+
+			return JsonSerializer.Serialize(transmissionResponse);
 		}
 	}
 }

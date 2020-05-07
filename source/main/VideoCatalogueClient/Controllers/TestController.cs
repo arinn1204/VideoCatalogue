21303deae5192b5ea05e0b9213f5bd52b09fd4f4
Aaron Rinn <arinn1250@gmail.com>
2020-05-07 04:48:28 -0700

    Added bt client

--- source/main/VideoCatalogueClient/Controllers/TestController.cs
+++ source/main/VideoCatalogueClient/Controllers/TestController.cs
@@ -1,4 +1,5 @@
-﻿using GrainsInterfaces.CodecParser;
+﻿using GrainsInterfaces.BitTorrentClient;
+using GrainsInterfaces.CodecParser;
 using GrainsInterfaces.VideoApi;
 using GrainsInterfaces.VideoLocator;
 using Microsoft.AspNetCore.Mvc;
@@ -10,15 +11,18 @@ namespace VideoCatalogueClient.Controllers
 	[Produces("application/json")]
 	public class TestController : Controller
 	{
+		private readonly IBitTorrentClient _btClient;
 		private readonly IParser _parser;
 		private readonly ISearcher _searcher;
 		private readonly IVideoApi _videoApi;
 
 		public TestController(
+			IBitTorrentClient btClient,
 			IParser parser,
 			IVideoApi videoApi,
 			ISearcher searcher)
 		{
+			_btClient = btClient;
 			_parser = parser;
 			_videoApi = videoApi;
 			_searcher = searcher;

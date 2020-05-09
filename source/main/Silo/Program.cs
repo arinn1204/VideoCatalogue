99994091848d820b59910ffc09c6ba77083fac1a
Aaron Rinn <arinn1250@gmail.com>
2020-05-08 17:48:30 -0700

    Configuring services and adding json file to IConfiguration

--- source/main/Silo/Program.cs
+++ source/main/Silo/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Threading.Tasks;
 using Grains.VideoInformation;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Orleans;
 using Orleans.Configuration;
@@ -42,16 +44,34 @@ namespace Silo
 					              opt.ClusterId = "Dev";
 					              opt.ServiceId = "OrleansBasic";
 				              })
+			             .ConfigureAppConfiguration(
+				              (ctx, builder) =>
+				              {
+					              builder.AddJsonFile("settings.json");
+				              })
 			             .ConfigureApplicationParts(
 				              parts => parts.AddApplicationPart(typeof(TheMovieDatabase).Assembly)
 				                            .WithReferences())
 			             .ConfigureLogging(log => log.AddConsole())
+			             .ConfigureServices(ConfigureServices)
 			             .UseLocalhostClustering();
 
 			var silo = builder.Build();
 			await silo.StartAsync();
 
 			return silo;
+		}
+
+		private static void ConfigureServices(
+			HostBuilderContext context,
+			IServiceCollection serviceCollection)
+		{
+			var configuration = new ConfigurationBuilder()
+			                   .AddJsonFile("settings.json")
+			                   .Build();
+
+			var startup = new Startup(configuration);
+			startup.ConfigureServices(serviceCollection);
 		}
 	}
 }

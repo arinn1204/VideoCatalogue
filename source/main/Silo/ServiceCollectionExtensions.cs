1d98c27dfe4114cfd03b29cfcda7c0f3f929ec92
Aaron Rinn <arinn1250@gmail.com>
2020-05-08 17:50:39 -0700

    Generic http client method that will add will fluently add http clients to registration

---
+++ source/main/Silo/ServiceCollectionExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Silo
+{
+	public static class ServiceCollectionExtensions
+	{
+		public static IServiceCollection AddHttpClient(
+			this IServiceCollection serviceCollection,
+			IConfiguration config,
+			string nameOfConfiguration)
+		{
+			var baseUrl = config.GetSection(nameOfConfiguration).Value;
+
+			var baseUri = new Uri(baseUrl);
+			return serviceCollection.AddHttpClient(
+				                         nameOfConfiguration,
+				                         client => client.BaseAddress = baseUri)
+			                        .Services;
+		}
+	}
+}

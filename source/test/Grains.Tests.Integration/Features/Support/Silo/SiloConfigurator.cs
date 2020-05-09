63f3332489f70442235e1243fd9bc4921c834c08
Aaron Rinn <arinn1250@gmail.com>
2020-05-08 17:51:08 -0700

    added silo configuration object

---
+++ source/test/Grains.Tests.Integration/Features/Support/Silo/SiloConfigurator.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Extensions.Configuration;
+using Orleans.Hosting;
+using Orleans.TestingHost;
+using Silo;
+
+namespace Grains.Tests.Integration.Features.Support.Silo
+{
+	public class SiloConfigurator : ISiloConfigurator
+	{
+		private readonly IConfiguration _configuration;
+
+		public SiloConfigurator()
+		{
+			_configuration = Hooks.Hooks.BuildConfiguration();
+		}
+
+#region ISiloConfigurator Members
+
+		public void Configure(ISiloBuilder hostBuilder)
+		{
+			hostBuilder
+			   .ConfigureServices(
+					serviceContainer =>
+					{
+						var configuration = new ConfigurationBuilder()
+						                   .AddConfiguration(_configuration)
+						                   .AddConfiguration(hostBuilder.GetConfiguration())
+						                   .Build();
+
+						var startup = new Startup(configuration);
+						startup.ConfigureServices(serviceContainer);
+					});
+		}
+
+#endregion
+	}
+}

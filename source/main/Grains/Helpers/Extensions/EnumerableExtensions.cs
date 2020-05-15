6e0411f36c37ce9f2d3d3d6b676b1a77b57af9c9
Aaron Rinn <arinn1250@gmail.com>
2020-05-15 06:52:53 -0700

    removed unused code

--- source/main/Grains/Helpers/Extensions/EnumerableExtensions.cs
+++ source/main/Grains/Helpers/Extensions/EnumerableExtensions.cs
@@ -5,20 +5,6 @@ namespace Grains.Helpers.Extensions
 {
 	public static class EnumerableExtensions
 	{
-		public static IEnumerable<TResult> Throw<TResult, TException>(
-			this IEnumerable<TResult> @this,
-			TException exceptionToThrow)
-			where TException : Exception
-		{
-			using var enumerator = @this.GetEnumerator();
-
-			while (enumerator.MoveNext())
-			{
-				yield return enumerator.Current;
-				throw exceptionToThrow;
-			}
-		}
-
 		public static async IAsyncEnumerable<TResult> Throw<TResult, TException>(
 			this IAsyncEnumerable<TResult> @this,
 			TException exceptionToThrow)

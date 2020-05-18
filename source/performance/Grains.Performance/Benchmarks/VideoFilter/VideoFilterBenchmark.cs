16bc502533898a33e5575cf80cb20cc47662d43a
Aaron Rinn <arinn1250@gmail.com>
2020-05-18 04:58:47 -0700

    Added benchmark for searcher and filter, filter is calling the searcher during setup to get all files on drive

---
+++ source/performance/Grains.Performance/Benchmarks/VideoFilter/VideoFilterBenchmark.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading.Tasks;
+using BenchmarkDotNet.Attributes;
+using GrainsInterfaces.VideoFilter;
+using GrainsInterfaces.VideoLocator;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Grains.Performance.Benchmarks.VideoFilter
+{
+	public class VideoFilterBenchmark : BenchmarkSetup
+	{
+		private string[] _files;
+
+		[GlobalSetup]
+		public async Task SetupFiles()
+		{
+			_files = await Services.GetRequiredService<ISearcher>().FindFiles(@"Y:\");
+		}
+
+		[Benchmark]
+		public async Task GetAllFiles()
+		{
+			var repo = Services.GetRequiredService<IVideoFilter>();
+			await repo.GetAcceptableFiles(_files);
+		}
+	}
+}
---
+++ source/performance/Grains.Performance/Benchmarks/VideoLocator/SearcherBenchmark.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading.Tasks;
+using BenchmarkDotNet.Attributes;
+using GrainsInterfaces.VideoLocator;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Grains.Performance.Benchmarks.VideoLocator
+{
+	public class SearcherBenchmark : BenchmarkSetup
+	{
+		[Benchmark]
+		public async Task GetAllFiles()
+		{
+			var repo = Services.GetRequiredService<ISearcher>();
+			await repo.FindFiles(@"Y:\");
+		}
+
+		[Benchmark]
+		public async Task SearchOneDirectory()
+		{
+			var repo = Services.GetRequiredService<ISearcher>();
+			await repo.FindFiles(@"Y:\Its.Always.Sunny.in.Philadelphia.S01-S12.DVDRip.XviD-SCENE");
+		}
+	}
+}

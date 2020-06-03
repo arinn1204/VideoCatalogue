2972628119b375038ee9d3ee06b95e94df30f51e
Aaron Rinn <arinn1250@gmail.com>
2020-06-03 08:42:28 -0700

    added interface for startup

---
+++ source/main/Client/Interfaces/IStartup.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Client.Interfaces
+{
+	public interface IStartup
+	{
+		IServiceCollection ConfigureServices(IServiceCollection serviceCollection);
+	}
+}

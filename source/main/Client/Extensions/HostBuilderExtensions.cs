0f7cc28b123dd1a29160e3837bec107c7e5a2d68
Aaron Rinn <arinn1250@gmail.com>
2020-06-03 08:42:21 -0700

    Added a method to add startups to the host builder

---
+++ source/main/Client/Extensions/HostBuilderExtensions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Client.Interfaces;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+
+namespace Client.Extensions
+{
+	public static class HostBuilderExtensions
+	{
+		public static IHostBuilder UseStartup<TStartup>(this IHostBuilder hostBuilder)
+			where TStartup : IStartup
+		{
+			hostBuilder.ConfigureServices(
+				(context, collection) =>
+				{
+					var startupCtor = typeof(TStartup)
+					   .GetConstructor(
+							new[]
+							{
+								typeof(IConfiguration)
+							});
+
+					var startup = startupCtor?.Invoke(
+						              new[]
+						              {
+							              context.Configuration
+						              }) as IStartup ??
+					              throw new Exception(
+						              $"Cannot create type {typeof(TStartup).FullName}");
+
+					startup.ConfigureServices(collection);
+				});
+
+			return hostBuilder;
+		}
+	}
+}

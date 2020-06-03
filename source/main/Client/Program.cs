988b2e4206a0917ba2e50f928dad370ce50a4c7d
Aaron Rinn <arinn1250@gmail.com>
2020-06-03 08:40:57 -0700

    Added methods to build the host builder and to start it

--- source/main/Client/Program.cs
+++ source/main/Client/Program.cs
@@ -1,12 +1,33 @@
-﻿using System;
+﻿using System.Threading.Tasks;
+using Client.Extensions;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
 
 namespace Client
 {
 	public class Program
 	{
-		private static void Main(string[] args)
+		public static async Task Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			await BuildHost(args)
+			     .Build()
+			     .StartAsync();
+		}
+
+		public static IHostBuilder BuildHost(string[] args)
+		{
+			return new HostBuilder()
+			      .UseConsoleLifetime()
+			      .UseStartup<Startup>()
+			      .ConfigureAppConfiguration(
+				       (context, configurationBuilder) =>
+				       {
+					       configurationBuilder
+						      .AddJsonFile("appsettings.json")
+						      .AddJsonFile($"appsettings.{context.HostingEnvironment}.json", true)
+						      .AddCommandLine(args)
+						      .AddEnvironmentVariables();
+				       });
 		}
 	}
 }

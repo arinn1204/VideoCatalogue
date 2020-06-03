a48b43f74a3f29660258ff960e3dd12672022497
Aaron Rinn <arinn1250@gmail.com>
2020-06-03 08:42:06 -0700

    Added extension method to build a connection string, allowing for a password to be stored separately from the connection string

---
+++ source/main/Client/Extensions/ConnectionStringExtensions.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace Client.Extensions
+{
+	public static class ConnectionStringExtensions
+	{
+		private const string CipherKey = "__PASSKEY_TOKEN__";
+
+		public static string BuildConnectionString(
+			this IConfiguration configuration,
+			string connectionStringKey)
+		{
+			var connectionString = configuration.GetConnectionString(connectionStringKey);
+
+			if (!connectionString.Contains(CipherKey))
+			{
+				return connectionString;
+			}
+
+			var password = configuration
+			              .GetSection($"ConnectionStrings:{connectionString}:password")
+			              .Value;
+			connectionString = connectionString.Replace(CipherKey, password);
+			return connectionString;
+		}
+	}
+}

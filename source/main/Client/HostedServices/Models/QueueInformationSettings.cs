748d638b11fa8f964d92ceb4a257bc9d74ffa7d1
Aaron Rinn <arinn1250@gmail.com>
2020-06-03 08:43:01 -0700

    Created the skeleton for the hosted service, this will read from the message queue and process all the messaged. If successful it will then delete the read messages

---
+++ source/main/Client/HostedServices/Models/QueueInformationSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace Client.HostedServices.Models
+{
+	public class QueueInformationSettings
+	{
+		public double TimeoutValue { get; set; }
+		public string QueueName { get; set; }
+	}
+}
---
+++ source/main/Client/HostedServices/Services/VideoQueueWorker.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+using Azure.Storage.Queues;
+using Azure.Storage.Queues.Models;
+using Client.HostedServices.Models;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
+
+namespace Client.HostedServices.Services
+{
+	public class VideoQueueWorker : IHostedService
+	{
+		private readonly QueueInformationSettings _options;
+		private readonly QueueClient _queueClient;
+
+		public VideoQueueWorker(
+			IOptions<QueueInformationSettings> options,
+			QueueClient queueClient)
+			=> (_options, _queueClient) = (options.Value, queueClient);
+
+#region IHostedService Members
+
+		public async Task StartAsync(CancellationToken cancellationToken)
+		{
+			var timeout = _options.TimeoutValue;
+
+			var response =
+				await _queueClient.CreateIfNotExistsAsync(cancellationToken: cancellationToken);
+
+			if (response.Status != (int) HttpStatusCode.OK)
+			{
+				return;
+			}
+
+			while (await _queueClient.ExistsAsync(cancellationToken))
+			{
+				var responses = await _queueClient.ReceiveMessagesAsync(
+					1,
+					TimeSpan.FromMinutes(timeout),
+					cancellationToken);
+
+				if (responses.Value.Length <= 0)
+				{
+					return;
+				}
+
+				var messages = responses.Value;
+
+				await ProcessMessage(messages, cancellationToken);
+
+				var deleteMessages = messages.Aggregate(
+					Enumerable.Empty<Task>(),
+					(current, message) => current.Append(
+						_queueClient.DeleteMessageAsync(
+							message.MessageId,
+							message.PopReceipt,
+							cancellationToken)));
+
+				await Task.WhenAll(deleteMessages);
+			}
+		}
+
+		public Task StopAsync(CancellationToken cancellationToken)
+		{
+			return Task.CompletedTask;
+		}
+
+#endregion
+
+		private async Task ProcessMessage(
+			IEnumerable<QueueMessage> message,
+			CancellationToken cancellationToken)
+		{
+			await Task.CompletedTask;
+		}
+	}
+}

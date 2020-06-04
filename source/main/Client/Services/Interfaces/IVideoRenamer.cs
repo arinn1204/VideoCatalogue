86bc08cda1b349e60c6b7faab06de14fa898f10e
Aaron Rinn <arinn1250@gmail.com>
2020-06-03 17:36:28 -0700

    Created renaming service to process the incoming messages

---
+++ source/main/Client/Services/Interfaces/IVideoRenamer.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Azure.Storage.Queues.Models;
+
+namespace Client.Services.Interfaces
+{
+	public interface IVideoRenamer
+	{
+		Task ProcessMessage(
+			IEnumerable<QueueMessage> message,
+			CancellationToken cancellationToken);
+	}
+}
---
+++ source/main/Client/Services/VideoRenamer.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Azure.Storage.Queues.Models;
+using Client.Services.Interfaces;
+using GrainsInterfaces.BitTorrentClient;
+using GrainsInterfaces.CodecParser;
+using GrainsInterfaces.VideoApi;
+using GrainsInterfaces.VideoFilter;
+using GrainsInterfaces.VideoLocator;
+
+namespace Client.Services
+{
+	public class VideoRenamer : IVideoRenamer
+	{
+		private readonly IBitTorrentClient _btClient;
+		private readonly IParser _parser;
+		private readonly ISearcher _searcher;
+		private readonly IVideoApi _videoApi;
+		private readonly IVideoFilter _videoFilter;
+
+		public VideoRenamer(
+			IBitTorrentClient bitTorrentClient,
+			IParser parser,
+			IVideoApi videoApi,
+			IVideoFilter videoFilter,
+			ISearcher searcher)
+			=> (_btClient, _parser, _videoApi, _videoFilter, _searcher) = (
+				bitTorrentClient, parser, videoApi, videoFilter, searcher);
+
+#region IVideoRenamer Members
+
+		public async Task ProcessMessage(
+			IEnumerable<QueueMessage> message,
+			CancellationToken cancellationToken)
+		{
+			await Task.CompletedTask;
+		}
+
+#endregion
+	}
+}
